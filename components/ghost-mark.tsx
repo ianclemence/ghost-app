cc8c7839269fac15b11446818157e2c68d8f4e76
Ian Clemence <batricupid@gmail.com>
2026-09-04 13:01:57 +0700

    Render Ghost artwork from ghost.svg in GhostMark

--- components/ghost-mark.tsx
+++ components/ghost-mark.tsx
@@ -6,7 +6,7 @@ import { Ghost } from "@/constants/theme";
 /**
  * Ghost visual mark.
  *
- * A simple, restrained ghost silhouette.
+ * Artwork from assets/images/ghost.svg, colorized via the color prop.
  * Quiet, editorial, timeless. Works at 16–64px.
  *
  * Usage:
@@ -29,9 +29,9 @@ export function GhostMark({ size = 24, color, style }: GhostMarkProps) {
         style,
       ]}
     >
-      <Svg width={size} height={size} viewBox="0 0 48 48" fill="none">
+      <Svg width={size} height={size} viewBox="0 0 32 32" fill="none">
         <Path
-          d="M24 4C15.2 4 8 11.2 8 20v16c0 1.6 1.3 2.9 2.9 2.9 2.4 0 4.3-1.9 4.3-4.3 0-1.2.5-2.3 1.3-3.1.4-.4 1-.6 1.6-.6 1.2 0 2.2 1 2.2 2.2v2.9c0 1.6 1.3 2.9 2.9 2.9s2.9-1.3 2.9-2.9v-2.9c0-1.2 1-2.2 2.2-2.2.6 0 1.2.2 1.6.6.8.8 1.3 1.9 1.3 3.1 0 2.4 1.9 4.3 4.3 4.3 1.6 0 2.9-1.3 2.9-2.9V20C40 11.2 32.8 4 24 4zM19 22c-1.6 0-2.9-1.3-2.9-2.9S17.4 16.2 19 16.2s2.9 1.3 2.9 2.9S20.6 22 19 22zm10 0c-1.6 0-2.9-1.3-2.9-2.9S27.4 16.2 29 16.2s2.9 1.3 2.9 2.9S30.6 22 29 22z"
+          d="M4.859 7.401v2.115h13.256v-4.231h-13.256v2.115zM22.91 7.401v2.115h4.231v-4.231h-4.231v2.115zM4.859 16.427v2.115h22.282v-4.231h-22.282v2.115zM4.859 25.311v1.974h8.744v-3.949h-8.744v1.975zM18.398 25.311v1.974h8.744v-3.949h-8.744v1.975z"
           fill={c}
         />
       </Svg>
